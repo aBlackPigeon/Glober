376d3b06de677fa6d326d48985b7666a607d20e3
aBlackPigeon <ablackpigeon7@gmail.com>
2025-12-30 19:18:13 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,35 @@
-
-
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-
+import {clerkMiddleware} from '@clerk/express';
+import { inngest } from './config/inngest.js';
+import { serve } from "inngest/express";
 
 const app = express();
+
+app.use(express.json()); 
+app.use(clerkMiddleware()); //req.auth will be availalbe in req object
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/' , (req,res) =>{
     res.send("API WORKING 123 ");
 })
 
-app.listen(ENV.PORT, () => {
-    console.log("Server started at port 5001");
-    connectDB();
-}) 
+const startServer = async() => {
+    try{
+        await connectDB();
+        if(ENV.NODE_ENV !== "production"){
+            app.listen(ENV.PORT , () => {
+                console.log("Server started on port" ,ENV.PORT);
+            });
+        }
+    }catch(error){
+        console.log("Error starting the server ", error);
+        process.exit(1);
+    }
+}
 
+startServer();
+
+export default app;
 
