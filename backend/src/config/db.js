e3f582e6225f952d2937d5df76ac7be66fcb928c
aBlackPigeon <ablackpigeon7@gmail.com>
2025-12-30 19:49:54 +0530

    fix **  mongoDb connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import {ENV} from './env.js'
 
 export const connectDB = async() => {
     try{
-        const conn = mongoose.connect(ENV.MONGO_URI);
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log("MongoDB connected successfully" , conn.connection.host);
     }catch(error){
         console.log("Error connecting to MongoDB " , error);
