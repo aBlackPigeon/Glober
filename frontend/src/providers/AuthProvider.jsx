e8a521a690392375fb4dd855a953dfcf2d2697c6
aBlackPigeon <ablackpigeon7@gmail.com>
2025-12-31 15:00:52 +0530

    AuthProvdier setup

---
+++ frontend/src/providers/AuthProvider.jsx
@@ -0,0 +1,37 @@
+import { useAuth } from "@clerk/clerk-react";
+import { createContext, useEffect } from "react";
+import { axiosInstance } from "../lib/axios";
+
+
+const AuthContext = createContext({});
+
+export default function AuthProvider({children}){
+    const {getToken} = useAuth();
+
+    useEffect(() => {
+        // setup axios interceptor
+        const interceptor = axiosInstance.interceptors.request.use(
+            async(config) => {
+                try{
+                    const token = await getToken();
+                    if(token) config.headers.Authorization = 'Bearer ${token}';
+                }catch(error){
+                    if(error.message?.includes("auth") || error.message?.includes("token")){
+                        toast.error("Authentication issue. Please refresh the page");
+                    }
+                    console.log("Error getting token" , error);
+                }
+                return config;
+            },
+            (error) => {
+                console.error("Axios error" , error);
+                return Promise.reject(error);
+            }
+
+        )
+        // cleanup function to remove the interceptor, avoid memory leaks
+        return () => axiosInstance.interceptors.request.eject(interceptor);
+    },[getToken]);
+
+    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+}
